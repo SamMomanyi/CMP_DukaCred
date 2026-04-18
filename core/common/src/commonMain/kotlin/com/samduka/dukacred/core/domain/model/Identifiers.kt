e0d278cebb67bc1dfcd0f8050c6489058cfe4af8
Samson Momanyi <samsonmomanyi2005@gmail.com>
2026-04-18 22:38:13 +0300

    identifiers

---
+++ core/common/src/commonMain/kotlin/com/samduka/dukacred/core/domain/model/Identifiers.kt
@@ -0,0 +1,16 @@
+package com.samduka.dukacred.core.domain.model
+
+import kotlin.jvm.JvmInline
+
+@JvmInline
+value class MerchantId(val value: String)
+@JvmInline
+value class SupplierId(val value: String)
+@JvmInline
+value class InvoiceId(val value: String)
+@JvmInline
+value class RequestId(val value: String)
+@JvmInline
+value class ContractId(val value: String)
+@JvmInline
+value class UserId(val value: String)
