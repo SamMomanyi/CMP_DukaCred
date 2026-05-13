a49a15e56a3fda4559c5f739564cb00a9dc92e6f
Samson Momanyi <samsonmomanyi2005@gmail.com>
2026-05-13 20:32:13 +0300

    android-camera permissions

--- feature/invoice-capture/src/iosMain/kotlin/com/samduka/dukacred/feature/invoicecapture/presentation/ui/InvoiceCaptureCamera.ios.kt
+++ feature/invoice-capture/src/iosMain/kotlin/com/samduka/dukacred/feature/invoicecapture/presentation/ui/InvoiceCaptureCamera.ios.kt
@@ -2,11 +2,15 @@ package com.samduka.dukacred.feature.invoicecapture.presentation.ui
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+
+
+// PEEKABOO IMPORTS
+import com.preat.peekaboo.ui.camera.CameraMode
+import com.preat.peekaboo.ui.camera.PeekabooCamera
 import com.preat.peekaboo.ui.camera.PeekabooCameraState
-
-
+import com.preat.peekaboo.ui.camera.rememberPeekabooCameraState
 actual class InvoiceCaptureCameraController(
-    private val state: PeekabooCameraState
+    val state: PeekabooCameraState
 ) {
     actual val isCameraReady: Boolean
         get() = state.isCameraReady
