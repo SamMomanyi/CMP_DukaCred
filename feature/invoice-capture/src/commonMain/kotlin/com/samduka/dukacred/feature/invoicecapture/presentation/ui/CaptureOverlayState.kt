5b8b67239c9222e327b3504571781d2baa10a46d
Samson Momanyi <samsonmomanyi2005@gmail.com>
2026-06-02 14:50:00 +0300

    no text detected state

--- feature/invoice-capture/src/commonMain/kotlin/com/samduka/dukacred/feature/invoicecapture/presentation/ui/CaptureOverlayState.kt
+++ feature/invoice-capture/src/commonMain/kotlin/com/samduka/dukacred/feature/invoicecapture/presentation/ui/CaptureOverlayState.kt
@@ -3,6 +3,7 @@ package com.samduka.dukacred.feature.invoicecapture.presentation.ui
 
 /** Transient post-capture feedback shown on top of the preview. */
 sealed interface CaptureWarning {
-    data object LowLight : CaptureWarning
-    data object Shaking  : CaptureWarning   // reserved for future real-time use
+    data object LowLight       : CaptureWarning
+    data object Shaking        : CaptureWarning
+    data object NoTextDetected : CaptureWarning
 }
