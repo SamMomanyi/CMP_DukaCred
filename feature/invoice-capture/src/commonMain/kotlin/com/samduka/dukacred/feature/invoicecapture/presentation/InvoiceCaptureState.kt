4c37f8b80a4a3dbb80a564dd1bed69ec5f5ade49
Samson Momanyi <samsonmomanyi2005@gmail.com>
2026-06-02 14:51:28 +0300

    autocapturing state addition

--- feature/invoice-capture/src/commonMain/kotlin/com/samduka/dukacred/feature/invoicecapture/presentation/InvoiceCaptureState.kt
+++ feature/invoice-capture/src/commonMain/kotlin/com/samduka/dukacred/feature/invoicecapture/presentation/InvoiceCaptureState.kt
@@ -1,21 +1,15 @@
 package com.samduka.dukacred.feature.invoicecapture.presentation
 
 import com.samduka.dukacred.feature.invoicecapture.domain.ParsedInvoice
-import com.samduka.dukacred.feature.invoicecapture.domain.model.ParsedInvoice
+
 
 /**
  * Represents the various states of the OCR scanning screen.
  */
 sealed interface InvoiceCaptureState {
-    // Waiting for the user to take a picture
-    data object Idle : InvoiceCaptureState
-
-    // Image captured, currently sending to AWS Bedrock/Gemini
-    data object Scanning : InvoiceCaptureState
-
-    // AI successfully read the invoice
-    data class Success(val invoice: ParsedInvoice) : InvoiceCaptureState
-
-    // Something went wrong (network error, blurry image)
-    data class Error(val message: String) : InvoiceCaptureState
+    data object Idle                                     : InvoiceCaptureState
+    data class  AutoCapturing(val countdown: Int)        : InvoiceCaptureState
+    data object Scanning                                 : InvoiceCaptureState
+    data class  Success(val invoice: ParsedInvoice)      : InvoiceCaptureState
+    data class  Error(val message: String)               : InvoiceCaptureState
 }
