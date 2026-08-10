a1b6abdfa502c716807d6a46841ee4b904f796b1
Samson Momanyi <samsonmomanyi2005@gmail.com>
2026-08-10 23:22:47 +0300

    DI

--- feature/invoice-capture/src/commonMain/kotlin/com/samduka/dukacred/feature/invoicecapture/di/InvoiceCaptureModule.kt
+++ feature/invoice-capture/src/commonMain/kotlin/com/samduka/dukacred/feature/invoicecapture/di/InvoiceCaptureModule.kt
@@ -1,16 +1,38 @@
 package com.samduka.dukacred.feature.invoicecapture.di
 
 
-import com.samduka.dukacred.feature.invoicecapture.presentation.InvoiceCaptureViewModel
+import com.samduka.dukacred.feature.invoicecapture.data.SupabaseInvoiceRepository
 import com.samduka.dukacred.feature.invoicecapture.domain.InvoiceImageCache
-import org.koin.compose.viewmodel.dsl.viewModel
+import com.samduka.dukacred.feature.invoicecapture.domain.repository.InvoiceRepository
+import com.samduka.dukacred.feature.invoicecapture.presentation.InvoiceCaptureViewModel
+import com.samduka.dukacred.feature.invoicecapture.presentation.InvoiceProcessingViewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val invoiceCaptureModule = module {
     single { InvoiceImageCache() }
 
-    viewModel { InvoiceCaptureViewModel(
-        imageCache = get()
-    ) }
 
+
+    single<InvoiceRepository> {
+        SupabaseInvoiceRepository(
+            ocrService = get(),
+            supabaseClient = get()
+        )
+    }
+
+
+    viewModel {
+        InvoiceProcessingViewModel(
+            imageCache = get(),
+            repository = get()
+        )
+    }
+
+
+    viewModel {
+        InvoiceCaptureViewModel(
+            imageCache = get()
+        )
+    }
 }
